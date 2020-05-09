a9ea7f48f4bfe9c5a4e0ceddf176e26ac7e78e10
info@phoza.net <you@example.cominfo@phoza.net>
2020-05-09 15:45:07 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from.models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+admin.site.register(Post, PostAdmin)
 # Register your models here.
